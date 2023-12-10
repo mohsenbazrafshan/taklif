09c085ae2f496a5b952bb1b4883c700cbc2ca528
mohsenbazrafshan <152017538+mohsenbazrafshan@users.noreply.github.com>
2023-12-10 19:43:17 +0330

    c#
    mohsenbazrafshan

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mohsenBazrafshan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 20; i++)
+            //Console.WriteLine(i);
+
+
+            //for (int i = 100; i <=999; i++)
+            //Console.WriteLine(i);
+
+
+            //int bimar=int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= bimar; i++)
+            //    Console.WriteLine(i);
+
+
+            // Console.Write("fard :");
+            //  for (int i = 1; i < 30; i += 2)
+            //  {
+            //      Console.Write("{0,5}" ,i);
+            //  }
+            //  Console.WriteLine("zooj :");
+            //  for (int a = 2; a < 30; a += 2)
+            //  {
+            //      Console.Write("{0,5}" ,a);
+            //  }
+
+
+            //for (int i = 2; i <= 30; i++)
+            //{
+            //    Console.Write("{0,5}",i);
+            //}
+
+
+            //for (int i = 2; i <= 30; i++)
+            //{
+            //    i++;
+            //    Console.Write("{0,5}",i);
+            //}
+
+
+            // for (int i = 5; i >= 1; i--)
+            //     Console.Write(i);
+
+
+            // for (int i = 0; i < 10; i += 3)
+            //     Console.Write(i);
+
+
+            // for (int i = 10; i >= 0; i-= 2)
+            //     Console.Write(i);
+
+
+            // int a = 10;
+            // for (int i = 1; i <= 5; i++)
+            // {
+            //     Console.WriteLine("{0} + {1}", i, a);
+            //     a--;
+            // }
+
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //        Console.WriteLine("hop");
+            //    else
+            //        Console.WriteLine(i);
+            //}
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0)
+            //        Console.WriteLine("hop");
+            //    else
+            //        Console.WriteLine(i);
+            //}
+
+
+            //int sum=0;
+            //int minute;
+            //for (int i = 1; i < 8; i++)
+            //{
+            //    Console.WriteLine("please rate the day {0} enter in minutes =",i);
+            //    minute = int.Parse(Console.ReadLine());
+            //    sum += minute;
+            //}
+            //Console.WriteLine("you studied for {0} minutes a week", sum);
+
+
+            //int sum = 0;
+            //int hours;
+            //for (int i = 1; i <= 4; i++)
+            //{
+            //    Console.WriteLine("please rate the week {0} enter in hour =", i);
+            //    hours = int.Parse(Console.ReadLine());
+            //    sum += hours;
+            //}
+            //Console.WriteLine("you studied for {0} hours a month", sum);
+
+
+            //int sum = 0;
+            //int hours;
+            //int i;
+            //for (i = 1; i <= 4; i++)
+            //{
+            //    Console.WriteLine("please rate the week {0} enter in hour =", i);
+            //    hours = int.Parse(Console.ReadLine());
+            //    sum += hours;
+            //}
+            //sum /= i - 1;
+            //Console.WriteLine("avarage hours in month : {0} ", sum);
+
+
+            //int score = 5;
+            //int month;
+            //Console.Write("enter number your month :");
+            //month = int.Parse(Console.ReadLine());
+            //Console.Clear();
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    int guess;
+            //    Console.Write("your guess?");
+            //    guess = int.Parse(Console.ReadLine());
+            //    if (guess == month)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Green;
+            //        Console.WriteLine("you win");
+            //        Console.WriteLine("your score: {0}", score);
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        score -= 1;
+            //        Console.WriteLine("try again  !");
+            //    }
+            //}
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
