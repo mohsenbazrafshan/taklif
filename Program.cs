c9c6cb94e2d0037c396ea5b8b6b035358fd10ec7
mohsenbazrafshan <152017538+mohsenbazrafshan@users.noreply.github.com>
2023-12-10 19:51:08 +0330

    C#
    mohsenbazrafshan

--- Program.cs
+++ Program.cs
@@ -4,107 +4,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace mohsen.bazrafshan
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //string mystr= Console.ReadLine();
-            ////for (int i = 0; i < mystr.Length; i++)
-            ////{
-            //// Console.WriteLine(mystr[i]);
-            ////}
-            ////ravesh2
-            //foreach (char item in mystr)
+ 
+        
+            //string[] name = new string [10] ;
+            //for (int i = 0; i <= 9; i++)             
             //{
-            //  Console.WriteLine(item);	
+            //    Console.WriteLine("please enther the name {0} : ", i+1);
+            //    name[i] = Console.ReadLine();                  
             //}
-            //for (int i =mystr.Length-1; i >= 0; i--)
+            
+
+            //string[] computer = new string[5] { "cpu", "mainboard", "ram", "hdd", "dvd" };
+            //int[] price = new int[5] { 100, 200, 300,   0, 500 };
+            //computer[3] = "keyboard";
+            //for (int i = 0; i <=4 ; i++)
             //{
-            // Console.WriteLine(mystr[i]);
+            //    Console.WriteLine(computer[i] + "   " + price[i]); 
             //}
+            //Console.SetCursorPosition(11,3);
+            //price[3] = int.Parse(Console.ReadLine());
 
 
-            //string name = "nima";
-            //int[] numeber = new int[5] { 10, 20, 30, 40, 50 };
-            //name = "mmad";
-            //numeber[3] = 55;
-            //for (int i = 0; i < 4; i++)
-            //    Console.WriteLine("your name is {0} and your {1}",name[i],numeber[i]);
-            //////foreach (char item in numeber)
+            //string[] week = { "saturday", "sunday", "monday", "tuesday", "wednesday", "thursday", "friday" };
+            //Console.Write("enter the number one day of week  (1-7):");
+            //byte num = byte.Parse(Console.ReadLine());
+            //if ((num < 1) || (num > 7))
+            //    Console.WriteLine("please enter right number");
+            //else
+            //    Console.WriteLine(week[--num]);
+               
+            
+           
+
+            //string[] week = { "saturday", "sunday", "monday", "tuesday", "wednesday", "thursday", "friday" };
+            ////Console.WriteLine(week[0]);
+            ////Console.WriteLine(week[1]);
+            ////Console.WriteLine(week[2]);
+            ////Console.WriteLine(week[3]);
+            ////Console.WriteLine(week[4]);
+            ////Console.WriteLine(week[5]);
+            ////Console.WriteLine(week[6]);
+
+
+            /////ravesh 2
+            //for (int i = 0; i < week.Length; i++)
             //{
-            //    Console.WriteLine(item); 
-            //}
-
-            //foreach (int item in name)
-            //{
-            //    Console.WriteLine(item); 
-            //}
-
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    Console.WriteLine(numeber[i]);
+            //    Console.WriteLine(week[i]);                
             //}
 
 
-            //int[]price=new int[50];
-            //for (int i = 0; i < 50; i++)
-            //{
-            //    Console.Write("enter the price of {0} the product : ", i + 1);
-            //    int temp = int.Parse(Console.ReadLine());
-            //    if (temp > 0)
-            //        price[i] = temp;
-            //    else
-            //        break;
-            //}
-            //    Console.WriteLine("product price plus VAT ");
-            //    Console.Clear();
-            //    for (int j = 0; j < price.Length; j++)
-            //    {                    
-            //        price[j] = (int)(price[j] * 0.09 + price[j]);                    
-            //        if
-            //            (price[j] > 0)
-            //            Console.WriteLine("product{0} : {1}", j, price[j]);
-            //    }
-
-
-
-
-
-            string[] car = { "prid", "mazda", "renault", "peugeot", "toyota" };
-            long[] price = { 100, 200, 300, 400, 5000 };
-            Console.Write("please enter car name for liner search : ");
-            string item = Console.ReadLine();
-            bool found = false;
-            for (int i = 0; i < car.Length; i++)
-                if (item == car[i])
-                {
-                    found = true;
-                    Console.WriteLine("{0} found in {1}", item, i);
-                    Console.WriteLine("price {0} = {1}", item, price[i]);
-                    break;
-                }
-            if (found == false)
+            string[] computer = new string[5];
+            long[] price = new long[5];
+            long sumprice = 0;
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("item is not found");
+                Console.WriteLine("enter the name of a computer part");
+                computer[i] = (Console.ReadLine());
+                Console.WriteLine("enter price of a computer part");
+                price[i] = long.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < price.Length; i++)
+            {
+                sumprice += price[i];
+                for (int j = 0; i < 5; i++)
+                {
+                    Console.WriteLine("{0}  {1}", computer[i], price[j]);
+                    Console.WriteLine(sumprice);
+                }
             }
 
 
-            //int[] number = { 20, 18, 10, 15, 50, 80, 10, 37 };
-            //foreach (int item in number)
-            //{
-            //    Console.Write(item+"/t");
-            //}
-            //Array.Sort(number);
-            //Array.Reverse(number);
-            //Console.WriteLine(Array.BinarySearch(number,10));
-
-
-
-
-
             Console.ReadKey();
+
         }
     }
 }
+
+
+     
